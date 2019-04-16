89011d735b2d45e4af3355601883c9d87ab3838f
陈海峰 <chenhaifeng@hualala.com>
2019-04-16 11:33:30 +0800

    从issus里查看比较好的方法

--- 2019-04-16.js
+++ 2019-04-16.js
@@ -20,3 +20,14 @@ const transAbs=()=>{
     }while (i<12);
     return targetData;
 }
+
+//看issue里的答案
+//Array.from(object, mapFunction, thisValue)
+// object	必需，要转换为数组的对象。
+// mapFunction	可选，数组中每个元素要调用的函数。
+// thisValue	可选，映射函数(mapFunction)中的 this 对象。
+let obj = {1:222, 2:123, 5:888}
+let arr = Array.from({length:12},(v,i) => obj[i+1] || null )
+
+let obj = {1:222, 2:123, 5:888};
+const result = Array.from({ length: 12 }).map((_, index) => obj[index + 1] || null);
