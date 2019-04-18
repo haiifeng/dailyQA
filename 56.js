e3d0ef4792cf23c0fcd8b0be113ab408e5a09ffe
chenhaifeng <chenhaifeng@hualala.com>
2019-04-18 14:59:36 +0800

    56天的类

--- 56.js
+++ 56.js
@@ -23,21 +23,62 @@
 * // 等待了10秒...
 * // I am eating junk food
 */
-class LazyMan{
-    constructor(params){
-        console.log(`Hi I am ${params}`);
-    }
-    sleep(params){
-        console.log(`等待了${params}秒`);
-        return this;
-    }
-    eat(params){
-        console.log(`I am eating ${params}`);
-        return this;
-    }
-    sleepFirst(params){
-        console.log(`等待了${params}秒`);
-        return this;
-    }
 
+class LazyManClass{
+    constructor(name){
+        this.taskList=[];
+        this.name=name;
+        console.log(`Hi I am ${this.name}`);
+        setTimeout(()=>{
+            this.next();
+        },0);
+    }
+    eat(name){
+        let that=this;
+        let fn=(function(n){
+            return function(){
+                console.log(`I am eating ${n}`);
+                that.next();
+            }
+        })(name);
+        this.taskList.push(fn);
+        return this;
+    }
+    sleepFirst(time){
+        let that=this;
+        let fn=(function(t){
+            return function(){
+                setTimeout(()=>{
+                    console.log(`等待了${t}秒...`);
+                    that.next();
+                },t*1000)
+            }
+        })(time);
+        this.taskList.unshift(fn);
+        return this;
+    }
+    sleep(time){
+        let that=this;
+        let fn=(function(t){
+            return function(){
+                setTimeout(()=>{
+                    console.log(`等待了${t}秒...`);
+                    that.next();
+                },t*1000);
+            }
+        })(time)
+        this.taskList.push(fn);
+        return this;
+    }
+    next(){
+        let fn=this.taskList.shift();
+        fn&&fn();
+    }
 }
+
+function LazyMan(name){
+    return new LazyManClass(name);
+}
+
+LazyMan('Tony').eat('lunch').eat('dinner').sleepFirst(5).sleep(4).eat('junk food');
+LazyMan('Tony').sleepFirst(5).sleep(4);
