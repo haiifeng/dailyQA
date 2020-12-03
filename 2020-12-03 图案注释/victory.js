c563c776bf5831e2d37116d1fe056cdfd0bb5087
陈海峰4 <chenhaifeng4@xdfcn>
2020-12-03 10:45:32 +0800

    图案注释

---
+++ 2020-12-03 图案注释/victory.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: haiifeng
+ * @Date: 2020-11-25 17:25:31
+ * @LastEditors: haiifeng
+ * @LastEditTime: 2020-11-25 17:26:42
+ * @FilePath: \code\img\be\app\public\js\comments\victory.js
+ * 
+ * victory
+ * 胜利手势
+ */
+/**                     d*##$.
+ * zP"""""$e.           $"    $o
+ *4$       '$          $"      $
+ *'$        '$        J$       $F
+ * 'b        $k       $>       $
+ *  $k        $r     J$       d$
+ *  '$         $     $"       $~
+ *   '$        "$   '$E       $
+ *    $         $L   $"      $F ...
+ *     $.       4B   $      $$$*"""*b
+ *     '$        $.  $$     $$      $F
+ *      "$       R$  $F     $"      $
+ *       $k      ?$ u*     dF      .$
+ *       ^$.      $$"     z$      u$$$$e
+ *        #$b             $E.dW@e$"    ?$
+ *         #$           .o$$# d$$$$c    ?F
+ *          $      .d$$#" . zo$>   #$r .uF
+ *          $L .u$*"      $&$$$k   .$$d$$F
+ *           $$"            ""^"$$$P"$P9$
+ *          JP              .o$$$$u:$P $$
+ *          $          ..ue$"      ""  $"
+ *         d$          $F              $
+ *         $$     ....udE             4B
+ *          #$    """"` $r            @$
+ *           ^$L        '$            $F
+ *             RN        4N           $
+ *              *$b                  d$
+ *               $$k                 $F
+ *               $$b                $F
+ *                 $""               $F
+ *                 '$                $
+ *                  $L               $
+ *                  '$               $
+ *                   $               $
+ */
