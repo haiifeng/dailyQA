90af08cd59ac7c0c81b2178aaa07ba1c6de1cb77
陈海峰4 <chenhaifeng4@xdfcn>
2021-07-21 23:01:19 +0800

    新增58和jd

---
+++ 2021-07-21 58jd/58.js
@@ -0,0 +1,22 @@
+// 58学车
+
+// 1.react类组件和函数组件的区别
+// 2.父子组件之间通信
+// 3.兄弟节点之间通信
+// 4.promise的优缺点
+// 5.async/await相对promis的优缺点
+// 6.generator
+// 7.vue的双向绑定原理
+// 8.跨域的原因
+// 9.jsonp的原理
+// 10.多维数组扁平化方法
+// 11.多个input，要求input值从小到大校验
+// 12.圣杯布局,float/flex/position/
+// 13.console.log(a) //输出undefined
+//   var a = 1
+// 14.console.log(a) //ReferenceError: a is not defined
+//   let a = 1
+// 15.const a=1;
+//   a=2; //Uncaught TypeError: Assignment to constant variable.
+// 16.const a={b:1};
+//   a.b=2; 不会报错，不推荐这么操作
---
+++ 2021-07-21 58jd/jd.js
@@ -0,0 +1,14 @@
+//京东-企业效率组
+// css方面
+// 圣杯布局 float/flex/absolute
+// 盒模型
+// box-sizeing使用的标准
+
+// js方面
+// 闭包
+// var/const/let
+// 如何实现换肤
+
+// react方面
+// 函数组件和类组件的区别
+// 组件之间传值
